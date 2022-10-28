e79f879b9afe0dff2a83e672ba4ccb1dac172dfb
Radiy <notron12460@gmail.com>
2022-10-28 10:52:49 +0500

    Working on TextBox
    not finished yet

---
+++ AntennVerificator/AntennVerificator/Controls/MyTextBox.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms.Design;
+namespace AntennVerificator
+{
+    public class MyTextBox : Control
+    {
+        #region -- Variables --
+        StringFormat SF = new StringFormat();
+
+        int topBorderOffset = 0;
+
+        TextBox tbInput = new TextBox();
+
+        Animation LocationTextPreviewAnim = new Animation();
+        Animation FontSizeTextPreviewAnim = new Animation();
+        #endregion
+
+        #region -- Properties --
+        public string TextPreview { get; set; } = "Input Text";
+        public Font FontTextPreview { get; set; } = new Font("Arial", 8, FontStyle.Bold);
+        public Color BorderColor { get; set; } = Color.Blue;
+        public Color BorderColorNotActive { get; set; } = Color.DarkBlue;
+
+        public string TextInput
+        {
+            get => tbInput.Text;
+            set => tbInput.Text = value;
+        }
+
+        [Browsable(false)]
+        public new string Text { get; set; }
+        #endregion
+
+        public MyTextBox()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.SupportsTransparentBackColor | ControlStyles.UserPaint, true);
+            DoubleBuffered = true;
+
+            Size = new Size(150, 40);
+            Font = new Font("Arial", 11.25F, FontStyle.Regular);
+            ForeColor = Color.Black;
+            BackColor = Color.White;
+
+            SF.Alignment = StringAlignment.Center;
+            SF.LineAlignment = StringAlignment.Center;
+
+            AdjustTextBoxInput();
+            this.Controls.Add(tbInput);
+
+            LocationTextPreviewAnim.Value = tbInput.Location.Y;
+            FontSizeTextPreviewAnim.Value = Font.Size;
+        }
+
+        protected override void OnCreateControl()
+        {
+            base.OnCreateControl();
+            tbInput.Text = Text;
+        }
+
+        private void AdjustTextBoxInput()
+        {
+            tbInput = new TextBox
+            {
+                Name = "InputBox",
+                BorderStyle = BorderStyle.None,
+                BackColor = BackColor,
+                ForeColor = ForeColor,
+                Font = Font
+            };
+
+            int offset = TextRenderer.MeasureText(TextPreview, FontTextPreview).Height / 2;
+            tbInput.Location = new Point(5, Height / 2 - offset);
+            tbInput.Size = new Size(Width - 10, tbInput.Height);
+
+            tbInput.LostFocus += TbInputLostFocus;
+        }
+
+        private void TbInputLostFocus(object sender, EventArgs e)
+        {
+            TextPreviewAction(false); 
+        }
+
+        #region -- Properties Update --
+        protected override void OnBackColorChanged(EventArgs e)
+        {
+            base.OnBackColorChanged(e);
+            tbInput.BackColor = BackColor;
+        }
+        protected override void OnForeColorChanged(EventArgs e)
+        {
+            base.OnForeColorChanged(e);
+            tbInput.ForeColor = ForeColor;
+        }
+        protected override void OnFontChanged(EventArgs e)
+        {
+            base.OnFontChanged(e);
+            tbInput.Font = Font;
+        }
+        #endregion
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics graph = e.Graphics;
+            graph.SmoothingMode = SmoothingMode.HighQuality;
+            graph.Clear(Parent.BackColor);
+
+            topBorderOffset = graph.MeasureString(TextPreview, FontTextPreview).ToSize().Height / 2;
+
+            Font FontTextPreviewActual = new Font(FontTextPreview.FontFamily, FontSizeTextPreviewAnim.Value, FontTextPreview.Style);
+
+            if (tbInput.Visible == false && FontTextPreviewActual.Size <= FontTextPreview.Size)
+            {
+                tbInput.Visible = true;
+                tbInput.Focus();
+            }
+            else if (tbInput.Visible == true && FontTextPreviewActual.Size <= FontTextPreview.Size)
+                tbInput.Visible = false;
+
+            Rectangle rectBase = new Rectangle(0, topBorderOffset, Width - 1, Height - 1 - topBorderOffset);
+
+            Size TextPreviewRectSize = graph.MeasureString(TextPreview, FontTextPreviewActual).ToSize();
+            Rectangle rectTextPreview = new Rectangle(5, (int)LocationTextPreviewAnim.Value, TextPreviewRectSize.Width + 3, TextPreviewRectSize.Height);
+
+            //Stroke
+            graph.DrawRectangle(new Pen(tbInput.Focused == true ? BorderColor : BorderColorNotActive), rectBase);
+
+            //Header/Discription
+            graph.DrawRectangle(new Pen(Parent.BackColor), rectTextPreview);
+            graph.FillRectangle(new SolidBrush(Parent.BackColor), rectTextPreview);
+
+            //Back
+            graph.FillRectangle(new SolidBrush(BackColor), rectBase);
+
+            graph.DrawString(TextPreview, FontTextPreviewActual, new SolidBrush(tbInput.Focused == true ? BorderColor : BorderColorNotActive), rectTextPreview);
+        }
+
+        private void TextPreviewAction(bool onTop)
+        {
+            if (onTop)
+            {
+                if (tbInput.Visible == false)
+                {
+                    LocationTextPreviewAnim = new Animation("TextPreviewLocation" + Handle, Invalidate, LocationTextPreviewAnim.Value, 0);
+                    FontSizeTextPreviewAnim = new Animation("TextPreviewFontSize" + Handle, Invalidate, FontSizeTextPreviewAnim.Value, FontTextPreview.Size);
+                }
+                else
+                {
+                    tbInput.Focus();
+                    return;
+                }
+            }
+            else
+            {
+                if (TextInput.Length == 0)
+                {
+                    LocationTextPreviewAnim = new Animation("TextPreviewLocation" + Handle, Invalidate, LocationTextPreviewAnim.Value, tbInput.Location.Y);
+                    FontSizeTextPreviewAnim = new Animation("TextPreviewFontSize" + Handle, Invalidate, FontSizeTextPreviewAnim.Value, Font.Size);
+                }
+                else
+                    return;
+            }
+
+            LocationTextPreviewAnim.stepDivider = 4;
+            FontSizeTextPreviewAnim.stepDivider = 4;
+
+            Animator.Request(LocationTextPreviewAnim, true);
+            Animator.Request(FontSizeTextPreviewAnim, true);
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+
+            TextPreviewAction(true);
+        }
+    }
+}
--- AntennVerificator/AntennVerificator/Form1.Designer.cs
+++ AntennVerificator/AntennVerificator/Form1.Designer.cs
@@ -30,103 +30,34 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.myCard3 = new AntennVerificator.MyCard();
-            this.myCard2 = new AntennVerificator.MyCard();
-            this.myCard1 = new AntennVerificator.MyCard();
             this.myToggleSwitch1 = new AntennVerificator.MyToggleSwitch();
             this.myButton3 = new AntennVerificator.MyButton();
             this.myButton2 = new AntennVerificator.MyButton();
             this.myButton1 = new AntennVerificator.MyButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.myCard3 = new AntennVerificator.MyCard();
+            this.myCard2 = new AntennVerificator.MyCard();
+            this.myCard1 = new AntennVerificator.MyCard();
+            this.myTextBox1 = new AntennVerificator.MyTextBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.panel1.Controls.Add(this.myToggleSwitch1);
             this.panel1.Controls.Add(this.myButton3);
             this.panel1.Controls.Add(this.myButton2);
             this.panel1.Controls.Add(this.myButton1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(224, 515);
             this.panel1.TabIndex = 4;
             // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.myCard3);
-            this.panel2.Controls.Add(this.myCard2);
-            this.panel2.Controls.Add(this.myCard1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(224, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(665, 515);
-            this.panel2.TabIndex = 5;
-            // 
-            // myCard3
-            // 
-            this.myCard3.BackColor = System.Drawing.Color.White;
-            this.myCard3.BackColorCurtain = System.Drawing.Color.SlateGray;
-            this.myCard3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.myCard3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.myCard3.Font = new System.Drawing.Font("Verdana", 9F);
-            this.myCard3.FontDescription = new System.Drawing.Font("Verdana", 8.25F);
-            this.myCard3.FontHeader = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
-            this.myCard3.ForeColorDescription = System.Drawing.Color.Black;
-            this.myCard3.ForeColorHeader = System.Drawing.Color.White;
-            this.myCard3.Location = new System.Drawing.Point(442, 12);
-            this.myCard3.Name = "myCard3";
-            this.myCard3.Size = new System.Drawing.Size(212, 243);
-            this.myCard3.strokeColor = System.Drawing.Color.Gray;
-            this.myCard3.TabIndex = 2;
-            this.myCard3.Text = "1234567890";
-            this.myCard3.TextDescription = "Antenna 3 description";
-            this.myCard3.TextHeader = "Antenna 3";
-            // 
-            // myCard2
-            // 
-            this.myCard2.BackColor = System.Drawing.Color.White;
-            this.myCard2.BackColorCurtain = System.Drawing.Color.SlateGray;
-            this.myCard2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.myCard2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.myCard2.Font = new System.Drawing.Font("Verdana", 9F);
-            this.myCard2.FontDescription = new System.Drawing.Font("Verdana", 8.25F);
-            this.myCard2.FontHeader = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
-            this.myCard2.ForeColorDescription = System.Drawing.Color.Black;
-            this.myCard2.ForeColorHeader = System.Drawing.Color.White;
-            this.myCard2.Location = new System.Drawing.Point(224, 12);
-            this.myCard2.Name = "myCard2";
-            this.myCard2.Size = new System.Drawing.Size(212, 243);
-            this.myCard2.strokeColor = System.Drawing.Color.Gray;
-            this.myCard2.TabIndex = 2;
-            this.myCard2.Text = "1234567890";
-            this.myCard2.TextDescription = "Antenna 2 description";
-            this.myCard2.TextHeader = "Antenna 2";
-            // 
-            // myCard1
-            // 
-            this.myCard1.BackColor = System.Drawing.Color.White;
-            this.myCard1.BackColorCurtain = System.Drawing.Color.SlateGray;
-            this.myCard1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.myCard1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.myCard1.Font = new System.Drawing.Font("Verdana", 9F);
-            this.myCard1.FontDescription = new System.Drawing.Font("Verdana", 8.25F);
-            this.myCard1.FontHeader = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
-            this.myCard1.ForeColorDescription = System.Drawing.Color.Black;
-            this.myCard1.ForeColorHeader = System.Drawing.Color.White;
-            this.myCard1.Location = new System.Drawing.Point(6, 12);
-            this.myCard1.Name = "myCard1";
-            this.myCard1.Size = new System.Drawing.Size(212, 243);
-            this.myCard1.strokeColor = System.Drawing.Color.Gray;
-            this.myCard1.TabIndex = 2;
-            this.myCard1.Text = "1234567890";
-            this.myCard1.TextDescription = "Antenna 1 description";
-            this.myCard1.TextHeader = "Antenna 1";
-            // 
             // myToggleSwitch1
             // 
+            this.myToggleSwitch1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.myToggleSwitch1.BackColor = System.Drawing.Color.SlateGray;
             this.myToggleSwitch1.backColorOn = System.Drawing.Color.MediumAquamarine;
             this.myToggleSwitch1.Checked = true;
@@ -168,16 +99,109 @@
             this.myButton1.TabIndex = 5;
             this.myButton1.Text = "myButton1";
             // 
+            // panel2
+            // 
+            this.panel2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.panel2.Controls.Add(this.myTextBox1);
+            this.panel2.Controls.Add(this.myCard3);
+            this.panel2.Controls.Add(this.myCard2);
+            this.panel2.Controls.Add(this.myCard1);
+            this.panel2.Location = new System.Drawing.Point(224, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(665, 515);
+            this.panel2.TabIndex = 5;
+            // 
+            // myCard3
+            // 
+            this.myCard3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.myCard3.BackColor = System.Drawing.Color.White;
+            this.myCard3.BackColorCurtain = System.Drawing.Color.SlateGray;
+            this.myCard3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.myCard3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.myCard3.Font = new System.Drawing.Font("Verdana", 9F);
+            this.myCard3.FontDescription = new System.Drawing.Font("Verdana", 8.25F);
+            this.myCard3.FontHeader = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.myCard3.ForeColorDescription = System.Drawing.Color.Black;
+            this.myCard3.ForeColorHeader = System.Drawing.Color.White;
+            this.myCard3.Location = new System.Drawing.Point(442, 12);
+            this.myCard3.Name = "myCard3";
+            this.myCard3.Size = new System.Drawing.Size(212, 243);
+            this.myCard3.strokeColor = System.Drawing.Color.Gray;
+            this.myCard3.TabIndex = 2;
+            this.myCard3.Text = "1234567890";
+            this.myCard3.TextDescription = "Antenna 3 description";
+            this.myCard3.TextHeader = "Antenna 3";
+            // 
+            // myCard2
+            // 
+            this.myCard2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.myCard2.BackColor = System.Drawing.Color.White;
+            this.myCard2.BackColorCurtain = System.Drawing.Color.SlateGray;
+            this.myCard2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.myCard2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.myCard2.Font = new System.Drawing.Font("Verdana", 9F);
+            this.myCard2.FontDescription = new System.Drawing.Font("Verdana", 8.25F);
+            this.myCard2.FontHeader = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.myCard2.ForeColorDescription = System.Drawing.Color.Black;
+            this.myCard2.ForeColorHeader = System.Drawing.Color.White;
+            this.myCard2.Location = new System.Drawing.Point(224, 12);
+            this.myCard2.Name = "myCard2";
+            this.myCard2.Size = new System.Drawing.Size(212, 243);
+            this.myCard2.strokeColor = System.Drawing.Color.Gray;
+            this.myCard2.TabIndex = 2;
+            this.myCard2.Text = "1234567890";
+            this.myCard2.TextDescription = "Antenna 2 description";
+            this.myCard2.TextHeader = "Antenna 2";
+            // 
+            // myCard1
+            // 
+            this.myCard1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.myCard1.BackColor = System.Drawing.Color.White;
+            this.myCard1.BackColorCurtain = System.Drawing.Color.SlateGray;
+            this.myCard1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.myCard1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.myCard1.Font = new System.Drawing.Font("Verdana", 9F);
+            this.myCard1.FontDescription = new System.Drawing.Font("Verdana", 8.25F);
+            this.myCard1.FontHeader = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.myCard1.ForeColorDescription = System.Drawing.Color.Black;
+            this.myCard1.ForeColorHeader = System.Drawing.Color.White;
+            this.myCard1.Location = new System.Drawing.Point(6, 12);
+            this.myCard1.Name = "myCard1";
+            this.myCard1.Size = new System.Drawing.Size(212, 243);
+            this.myCard1.strokeColor = System.Drawing.Color.Gray;
+            this.myCard1.TabIndex = 2;
+            this.myCard1.Text = "1234567890";
+            this.myCard1.TextDescription = "Antenna 1 description";
+            this.myCard1.TextHeader = "Antenna 1";
+            // 
+            // myTextBox1
+            // 
+            this.myTextBox1.BackColor = System.Drawing.Color.White;
+            this.myTextBox1.BorderColor = System.Drawing.Color.Blue;
+            this.myTextBox1.BorderColorNotActive = System.Drawing.Color.DarkBlue;
+            this.myTextBox1.Font = new System.Drawing.Font("Arial", 11.25F);
+            this.myTextBox1.FontTextPreview = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold);
+            this.myTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.myTextBox1.Location = new System.Drawing.Point(301, 322);
+            this.myTextBox1.Name = "myTextBox1";
+            this.myTextBox1.Size = new System.Drawing.Size(150, 40);
+            this.myTextBox1.TabIndex = 3;
+            this.myTextBox1.TextInput = "";
+            this.myTextBox1.TextPreview = "Input Text";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(889, 515);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(905, 554);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AntennVerificator";
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
@@ -195,6 +219,7 @@
         private MyCard myCard3;
         private MyCard myCard2;
         private MyToggleSwitch myToggleSwitch1;
+        private MyTextBox myTextBox1;
     }
 }
 
